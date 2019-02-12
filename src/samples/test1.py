e172b9371bbe3d6090dcd2eef0cea514fab0af26
smertiens <sean.mertiens@gmail.com>
2019-02-12 14:22:02 +0100

    Added threading and AsyncBranches Cleanup

--- src/samples/test1.py
+++ src/samples/test1.py
@@ -1,16 +1,16 @@
-from old import CLIInputNode
-from nodes import EchoOutputNode
-from Stream import *
+import logging
+
+from Stream import Stream
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    logging.disable(logging.CRITICAL)
 
-    st = Stream()
-    root = CLIInputNode.CLIInputNode("cli", {"save_to": "username", "prompt": "Enter your name"})
-    n2 = EchoOutputNode.EchoOutputNode("echo", {"text": "Your name is {Res::Text:username}"})
-    root.addChild(n2)
-    st.appendNode(root)
-    st.run()
+    stream = Stream()
+
+    stream.append_node()
+    stream.append_node()
+    stream.append_node()
+
+
 
     sr.run(st, root)
