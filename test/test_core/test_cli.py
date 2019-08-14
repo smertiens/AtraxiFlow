07bf8620823bfcd118062b85373c69e55444e4aa
smertiens <sean.mertiens@gmail.com>
2019-08-14 20:00:13 +0200

    Added cli tests

---
+++ test/test_core/test_cli.py
@@ -0,0 +1,41 @@
+#
+# AtraxiFlow - Flexible python workflow tool
+#
+# Copyright (C) 2019  Sean Mertiens
+# For more information on licensing see LICENSE file
+#
+
+from atraxiflow.atraxiflow_cli import *
+from atraxiflow.base.common import *
+from click.testing import CliRunner
+
+
+def test_no_command():
+    runner = CliRunner()
+    result = runner.invoke(cli)
+
+    assert result.exit_code == 0
+    assert 'Usage: ' in result.output
+
+
+def test_version():
+    runner = CliRunner()
+    result = runner.invoke(version)
+
+    assert result.exit_code == 0
+    assert result.output == ax_version + '\n'
+
+
+def test_run(tmp_path):
+    tmp_path = str(tmp_path)
+    f = os.path.join(tmp_path, 'test.way')
+
+    node1 = EchoOutputNode({'msg': 'Hello'})
+    node2 = EchoOutputNode({'msg': 'World'})
+    wayfiles.dump(f, [node1, node2])
+
+    runner = CliRunner()
+    result = runner.invoke(run, [f])
+    assert result.exit_code == 0
+
+    assert 'Hello\nWorld\n' in result.output
